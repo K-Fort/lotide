569d5b96dffa56b801f7d3867787cda374c540ed
K-Fort <fortink01@gmail.com>
2024-06-27 06:48:57 -0400

    implemented assertObjectEqual function

---
+++ assertObjectEqual.js
@@ -0,0 +1,44 @@
+const eqObjects = function (object1, object2) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (let key of keys1) {
+    const value1 = object1[key];
+    const value2 = object2[key];
+
+    if (Array.isArray(value1) && Array.isArray(value2)) {
+      if (!eqArrays(value1, value2)) {
+        return false;
+      }
+    } else if (value1 !== value2) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertObjectsEqual = function (actual, expected) {
+  const inspect = require("util").inspect;
+
+  if (eqObjects(actual, expected)) {
+    console.log(
+      `✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`,
+    );
+  } else {
+    console.log(`
+🛑🛑🛑  Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
+
+const sally = { hair: "long", hairColor: "brown", children: "2" };
+const susanne = { hair: "long", hairColor: "brown", children: "2" };
+const jeremy = { hair: "short", hairColor: "blond", children: "2" };
+const bob = { hair: "short", hairColor: "brown", children: "0" };
+
+assertObjectsEqual(sally, susanne);
+assertObjectsEqual(sally, bob);
+assertObjectsEqual(sally, jeremy);
