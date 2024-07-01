5f92a4b43d6dd2fe79228cc7ae112ba09efa2b62
K-Fort <fortink01@gmail.com>
2024-07-01 17:41:21 -0400

    modified headTest tests

--- test/headTest.js
+++ test/headTest.js
@@ -1,4 +1,5 @@
 const head = require("../head");
+const assertEqual = require(".../assertEqual");
 
-console.log(head([1, 2, 3]))
-console.log(head("a", "b", "c"))
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
