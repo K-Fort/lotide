f9bbf752c8dc010d2a5de98983ec8b54864081fd
K-Fort <fortink01@gmail.com>
2024-07-01 19:45:56 -0400

    changes

--- middle.js
+++ middle.js
@@ -1,23 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
-
 // Implement middle which will take in an array and return the middle-most element(s) of the given array.
 
 
@@ -36,11 +16,4 @@ const middle = function(arr) {
   return result;
 }
 
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
-
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+module.exports = middle;
