3ba34261570dd1cfb59ec9ee6b4dc1bd02b74222
K-Fort <fortink01@gmail.com>
2024-07-02 09:25:41 -0400

    changes

--- test/middleTest.js
+++ test/middleTest.js
@@ -4,10 +4,10 @@ const assertArraysEqual = require("../assertArraysEqual");
 
 describe("#middle", () => {
   it("returns [2] for [1, 2, 3]", () => {
-    assert.strictEqual(assertArraysEqual(middle([1, 2, 3]), [2]));
+    assert.deepEqual(assertArraysEqual(middle([1, 2, 3]), [2]));
   });
 
   it("returns ['c'] for ['a', 'b', 'c', 'd', 'e']", () => {
-    assert.strictEqual(assertArraysEqual(middle(['a', 'b', 'c', 'd', 'e']), ['c']));
+    assert.deepEqual(assertArraysEqual(middle(['a', 'b', 'c', 'd', 'e']), ['c']));
   });
 });
