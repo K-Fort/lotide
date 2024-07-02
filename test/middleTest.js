f28cfc34e98127c342c60dc4927d133340d2af47
K-Fort <fortink01@gmail.com>
2024-07-02 09:28:34 -0400

    changes

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,13 +1,13 @@
 const assert = require('chai').assert;
 const middle = require("../middle");
-const assertArraysEqual = require("../assertArraysEqual");
+
 
 describe("#middle", () => {
   it("returns [2] for [1, 2, 3]", () => {
-    assert.deepEqual(assertArraysEqual(middle([1, 2, 3]), [2]));
+    assert.deepEqual(middle([1, 2, 3]), [2]);
   });
 
   it("returns ['c'] for ['a', 'b', 'c', 'd', 'e']", () => {
-    assert.deepEqual(assertArraysEqual(middle(['a', 'b', 'c', 'd', 'e']), ['c']));
+    assert.deepEqual(middle(['a', 'b', 'c', 'd', 'e']), ['c']);
   });
 });
--- test/tailTest.js
+++ test/tailTest.js
@@ -4,14 +4,14 @@ const assert = require('chai').assert;
 describe("#tail", () => {
   it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
     const result = tail(["Hello", "Lighthouse", "Labs"]);
-    assert.deepEqual(result.length, 2);
-    assert.deepEqual(result[0], "Lighthouse");
-    assert.deepEqual(result[1], "Labs");
+    assert.strictEqual(result.length, 2);
+    assert.strictEqual(result[0], "Lighthouse");
+    assert.strictEqual(result[1], "Labs");
   });
 
   it("does not modify the original array", () => {
     const words = ["Yo Yo", "Lighthouse", "Labs"];
     tail(words);
-    assert.deepEqual(words.length, 3);
+    assert.strictEqual(words.length, 3);
   });
 });
